2df98d3edbb749bb9bcc9a317668197cdc006d4b
Lucas Moreira <moreirapontocom@gmail.com>
2019-09-23 09:13:30 -0300

    Dynamic CEP using TextField

--- lib/home.dart
+++ lib/home.dart
@@ -9,15 +9,25 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
 
+  String _resultado = "";
+
+  TextEditingController _controllerCEP = TextEditingController();
+
   _recuperarCep() async {
-    String cep = "30775330";
-    String url = "http://viacep.com.br/ws/" + cep + "/json/";
+
+    String url = "http://viacep.com.br/ws/" + _controllerCEP.text + "/json/";
 
     http.Response response = await http.get(url);
 
     Map<String, dynamic> content = json.decode(response.body);
 
-    print("Rua " + content["logradouro"] + ", " + content["bairro"] + " - " + content["localidade"]);
+    String logradouro = content["logradouro"];
+    String bairro = content["bairro"];
+    String localidade = content["localidade"];
+
+    setState(() {
+      _resultado = logradouro + ", " + bairro + " - " + localidade;
+    });
   }
 
   @override
@@ -29,9 +39,25 @@ class _HomeState extends State<Home> {
       ),
       body: Container(
         padding: EdgeInsets.all(40),
-        child: RaisedButton(
-          child: Text("Recuperar CEP"),
-          onPressed: _recuperarCep,
+        width: double.infinity,
+        child: Column(
+          children: <Widget>[
+
+            TextField(
+              keyboardType: TextInputType.number,
+              controller: _controllerCEP,
+              decoration: InputDecoration(
+                labelText: "Informe o CEP"
+              ),
+            ),
+
+            RaisedButton(
+              child: Text("Recuperar CEP"),
+              onPressed: _recuperarCep,
+            ),
+            Text( _resultado )
+
+          ],
         ),
       ),
     );
